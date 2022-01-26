3ab92e2b631af7a03727ec1af83fd5446b17d26c
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-01-26 12:05:19 +0800

    Update config.py

--- Music/config.py
+++ Music/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 get_queue = {}
 SESSION_NAME = getenv('SESSION_NAME', 'session')
 BOT_TOKEN = getenv('BOT_TOKEN')
-API_ID = int(getenv('API_ID', "1905050903"))
+API_ID = int(getenv('API_ID', "19173262"))
 API_HASH = getenv('API_HASH')
 DURATION_LIMIT = int(getenv('DURATION_LIMIT', '30'))
 COMMAND_PREFIXES = list(getenv('COMMAND_PREFIXES', '/ . , : ; !').split())
